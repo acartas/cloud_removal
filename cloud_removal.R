1537a332770e8de8cf3fda457350a7a4bb2d8ed7
Anika <anika.cartas@gmail.com>
2015-02-07 01:16:18 -0500

    Now able to compile matrices of desired output (second-highest value). Still missing ability to save to .jpg file.

--- cloud_removal.R
+++ cloud_removal.R
@@ -3,14 +3,10 @@ library(raster)
 filenames <- list.files("path08row60", full.names=TRUE)
 
 #try to read in the .jpg 3 bands #stack read multiple files, brick only reads one.
-r1 <- stack(filenames[1]) 
-r2 <- stack(filenames[2])
-
 rStack <- sapply(filenames,stack)
 
 
 keep <- c(1)
-nokeep <- vector()
 numFiles <- length(rStack)
 
 #can't figure out stupid apply function!!! loops it is!
@@ -21,28 +17,45 @@ for (cur in c(2:numFiles)){
 
 #discard any image with different extent
 rStack <- rStack[keep]
-sprintf("Using %d files for image collage.",num)
+numFiles <- length(rStack)
+sprintf("Using %d files for image collage.",numFiles)
 
-#display all images being used
-dispRow <- floor(sqrt(length(rStack)))
-dispCol <- ceiling(length(rStack)/nRow)
-par(mfrow=c(dispRow,dispCol))
-lapply(rStack,plotRGB)
-dev.off()
-
-#create blank stack for results
-outR <- raster(ext=rStack[[1]]@extent)
-outG <- raster(ext=rStack[[1]]@extent)
-outB <- raster(ext=rStack[[1]]@extent)
+#display all images being used (just for information, not needed!)
+#dispRow <- floor(sqrt(numFiles))
+#dispCol <- ceiling(numFiles/dispRow)
+#par(mfrow=c(dispRow,dispCol))
+#invisible(sapply(rStack,plotRGB))
+#dev.off()
 
 #row by row, grab the second-highest value and put it into output.
 nRow = nrow(rStack[[1]])
 nCol = ncol(rStack[[1]])
-for (i in c(1:nRow)){
-  curRowValues <- lapply(rStack,values,row=i) #stores RGB for row 1 for all images  
-  #proceed through the row
-  for (j in c(1:nCol)){
-    #curRowValues[[j]] is an nCol x 3 matrix
-      
+
+#create blank stack for results
+outR <- matrix(nrow=nRow,ncol=nCol)(#raster(ext=rStack[[1]]@extent, nrows=nRow, ncols=nCol)
+outG <- matrix(nrow=nRow,ncol=nCol)#raster(ext=rStack[[1]]@extent, nrows=nRow, ncols=nCol)
+outB <- matrix(nrow=nRow,ncol=nCol)#raster(ext=rStack[[1]]@extent, nrows=nRow, ncols=nCol)
+
+#should be 1:nRow and 1:nCol, set smaller for testing!
+for (curRow in c(4000:4100)){
+  
+  curRowValues <- lapply(rStack,values,row=curRow) #stores RGB for row 1 for all images  
+  for (curCol in c(2000:2100)){#proceed through column-by-column
+    red <- vector(mode="integer")
+    blue <- vector(mode="integer")
+    green <- vector(mode="integer")
+    for (curFile in c(1:numFiles)){ #gather values for current cell for all images
+      curRed   = curRowValues[[curFile]][[curCol,1]]
+      red <- c(red,curRed)
+      curGreen = curRowValues[[curFile]][[curCol,2]]
+      green <- c(green,curGreen)
+      curBlue  = curRowValues[[curFile]][[curCol,3]]
+      blue <- c(blue,curBlue)
+    }
+    outR[curRow,curCol] <- sort(red,partial=numFiles-1)[numFiles-1])
+    outG[curRow,curCol] <- sort(green,partial=numFiles-1)[numFiles-1]
+    outB[curRow,curCol] <- sort(blue, partial=numFiles-1)[numFiles-1]
+    cat("Row ",curRow," Col ",curCol,"\ --> "); 
+    cat("Red: ", outR[curRow,curCol], " Green: ", outG[curRow,curCol], " Blue: ", outB[curRow,curCol],"\n")
   }
 }
